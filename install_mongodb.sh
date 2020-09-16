6b93baca2c122da84ccd46ddff94ae571efbc92d
root <root@localhost.localdomain>
2020-09-16 18:17:10 +0300

    cloud-testapp

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -3,6 +3,8 @@ echo "add key"
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
 echo "add repo"
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+echo install https support
+apt-get install apt-transport-https ca-certificates
 echo "update apt"
 sudo apt-get update
 echo "install mongodb package"

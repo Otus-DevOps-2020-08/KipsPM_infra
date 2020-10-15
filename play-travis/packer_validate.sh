a999807678301d0d275e0e9e6295788261e744e1
root <root@localhost.localdomain>
2020-10-15 11:17:37 +0300

    ansible-3

--- play-travis/packer_validate.sh
+++ play-travis/packer_validate.sh
@@ -2,7 +2,9 @@
 
 echo "Validate packer"
 set -v
-packer validate -var-file=packer/variables.json.example packer/immutable.json
-packer validate -var-file=packer/variables.json.example packer/ubuntu16.json
+cd packer
+packer validate -var-file=variables.json.example immutable.json
+packer validate -var-file=variables.json.example ubuntu16.json
+cd ..
 packer validate -var-file=packer/variables.json.example packer/app.json
 packer validate -var-file=packer/variables.json.example packer/db.json
